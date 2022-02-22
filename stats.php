38ccfe79bd195be321ff1c97514fe03a90edb442
Brian Suda <brian@suda.co.uk>
2022-02-22 13:11:16 +0000

    Adding the output as CSV incase that's useful

--- stats.php
+++ stats.php
@@ -7,13 +7,19 @@ $contents = fread($handle, filesize($filename));
 fclose($handle);
 
 $bookmarks = json_decode($contents,true);
-
 //$bookmarks = array_slice($bookmarks,0,25);
+//if(http_response('http://algaelab.org')){
+//	print("T");
+//} else {
+//	print("F");
+//}
+//return;
+
 
 $years = array();
 $years_checked = array();
 $counter = 0;
-print("Found ".$bookmarks.count().' bookmarks to check');
+error_log("Found ".count($bookmarks).' bookmarks to check',0);
 foreach($bookmarks as $b){
 	$y = substr($b['time'],0,4);
 	if (!array_key_exists($y,$years)){
@@ -21,13 +27,14 @@ foreach($bookmarks as $b){
 		$years_checked[$y] = 0;
 	}
 	$years[$y]++;
+	error_log('Checking ('.++$counter.')',0);
 	if(http_response($b['href'])){
 		$years_checked[$y]+= 1;
 		print('✅');
 	} else {
 		print('❌');
 	}
-	print(' Checking ('.++$counter.'): '.$b['href'].' from '.$y."\n");
+	print(','.$y.',"'.$b['href'].'"'."\n");
 
 }
 
@@ -37,20 +44,20 @@ foreach($years as $k=>$v){
 	$total_urls += $v;
 	$total_checked += $years_checked[$k];
 
-	print($k."\n----\n");
-	print($years_checked[$k].'/'.$v.' = ');
+	error_log($k."\n".'----',0);
+	$line = ($years_checked[$k].'/'.$v.' = ');
 
 	if ($years_checked[$k] > 0){
-		print((($years_checked[$k]/$v)*100).'%');
+		$line .= ((($years_checked[$k]/$v)*100).'%');
 	} else {
-		print('0%');		
+		$line .= ('0%');		
 	}
-	print("\n");
+	error_log($line."\n",0);
 }
-print('TOTAL'."\n");
-print($total_checked.'/'.$total_urls.' = ');
-print((($total_checked/$total_urls)*100).'%');
-
+error_log('TOTAL',0);
+$line = ($total_checked.'/'.$total_urls.' = ');
+$line .= ((($total_checked/$total_urls)*100).'%');
+error_log($line,0);
 
 
 
@@ -62,6 +69,7 @@ function http_response($url, $status = null)
     curl_setopt($ch, CURLOPT_NOBODY, TRUE); // remove body 
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); 
     curl_setopt($ch,CURLOPT_TIMEOUT, 10);
+    curl_setopt($ch,CURLOPT_USERAGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.3 Safari/605.1.15");
     $head = curl_exec($ch); 
     $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE); 
 
